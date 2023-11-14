7ef47032bfd3154a33cc5a74061165296e31008c
Viktor Martynov <2013alfabravo@gmail.com>
2023-11-14 08:23:49 +0400

    rename project

--- Fitness Tracker API/stage1/test/FitnessTrackerTest.java
+++ Fitness Tracker API/stage1/test/FitnessTrackerTest.java
@@ -2,7 +2,7 @@ import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.stage.SpringTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 
-public class PublicApiTest extends SpringTest {
+public class FitnessTrackerTest extends SpringTest {
     @DynamicTest
     CheckResult test() {
         return CheckResult.correct();
